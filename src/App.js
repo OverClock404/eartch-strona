da1e4bcf1865ff837b5135fcc678ed7abebe8b4e
OverClock404 <aron.frysztacki24@gmail.com>
2022-04-17 20:26:56 +0200

    Co-authored-by: codeberry.official@gmail.com <codeberry.official@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">      
+      <div className="Window">
+        <div className="ProfileBorder">
+          <div className="Profile">
+            <div className="ProfileText">
+              <h1>EartchGaming</h1>
+            </div>
+          </div>
+        </div>
+       </div>
+    </div>
+  );
+}
+
+export default App;

42ebda84c4e84f3b7a05507fc4fc927a12592a96
CodeBerry <codeberry@Air-Aleksander.Dlink>
2022-04-19 10:03:06 +0200

    idk

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">      
-      <div className="Window">
-        <div className="ProfileBorder">
-          <div className="Profile">
+      <div className="window">
+        <div className="profileBorder">
+          <div className="profile">
             <div className="profileName">
               <h1>EartchGaming</h1>
             </div>
